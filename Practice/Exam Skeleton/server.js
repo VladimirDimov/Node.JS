fd2af5c63b693f196ffe98beccff3558fdf2d703
VladimirDimov <vdimov1124@gmail.com>
2016-01-14 18:53:29 +0200

    added error handling

--- Practice/Exam Skeleton/server.js
+++ Practice/Exam Skeleton/server.js
@@ -10,6 +10,16 @@ require('./server/config/mongoose')(config);
 require('./server/config/passport')(config);
 require('./server/config/routers/routes-loader')(app);
 
+// Handle 404
+app.use(function (req, res) {
+    res.send('404: Page not Found', 404);
+});
+  
+// Handle 500
+app.use(function (error, req, res, next) {
+    res.send('500: Internal Server Error', 500);
+});
+
 app.listen(config.port);
 
 console.log("Server running on port: " + config.port);
--- Practice/Exam Skeleton/server/config/express.js
+++ Practice/Exam Skeleton/server/config/express.js
@@ -5,18 +5,19 @@ var express = require('express'),
     busboy = require('connect-busboy'),
     passport = require('passport');
 
-module.exports = function(app, config) {
+module.exports = function (app, config) {
     app.set('view engine', 'jade');
     app.set('views', config.rootPath + '/server/views');
     app.use(cookieParser());
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(busboy({immediate: false}));
-    app.use(session({secret: 'magic unicorns', resave: true, saveUninitialized: true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(busboy({ immediate: false }));
+    app.use(session({ secret: 'magic unicorns', resave: true, saveUninitialized: true }));
     app.use(passport.initialize());
     app.use(passport.session());
     app.use(express.static(config.rootPath + '/public'));
-    app.use(function(req, res, next) {
+    
+    app.use(function (req, res, next) {
         if (req.session.error) {
             var msg = req.session.error;
             req.session.error = undefined;
@@ -28,14 +29,14 @@ module.exports = function(app, config) {
 
         next();
     });
-    
-     app.use(function (req, res, next) {
+
+    app.use(function (req, res, next) {
         if (req.user) {
             app.locals.currentUser = req.user;
         } else {
             app.locals.currentUser = undefined;
         }
-        
+
         next();
     });
 };
